25aaafa84d7d4b40caa40cd41d64b3fc75d4d2b4
Tremckinley <tremckinley901@gmail.com>
2024-08-02 23:31:06 -0500

    hard coded card and buttons

--- social-links-fementor/src/app/page.js
+++ social-links-fementor/src/app/page.js
@@ -2,19 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen bg-black text-white flex flex-col justify-center items-center">
+      <div className='contain flex flex-col bg-stone-800 p-3 items-center rounded'>
 
-      Jessica Randall
-      London, United Kingdom
-      "Front-end developer and avid reader."
+      <h2>Jessica Randall</h2>
+      <h3>London, United Kingdom</h3>
+      <p>"Front-end developer and avid reader."</p>
+      <button className="py-1 w-40 bg-stone-600 m-1 rounded text-xs">GitHub</button>
+      <button className="py-1 w-40 bg-stone-600 m-1 rounded text-xs">Frontend Mentor</button>
+      <button className="py-1 w-40 bg-stone-600 m-1 rounded text-xs">LinkedIn</button>
+      <button className="py-1 w-40 bg-stone-600 m-1 rounded text-xs">Twitter</button>
+      <button className="py-1 w-40 bg-stone-600 m-1 rounded text-xs">Instagram</button>
+      
+      
+      
+      
+      
+      </div>
 
-      GitHub
-      Frontend Mentor
-      LinkedIn
-      Twitter
-      Instagram
-
-      <div className="attribution">
+      <div className="attribution mt-10">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
         Coded by <a href="#">Your Name Here</a>.
       </div>

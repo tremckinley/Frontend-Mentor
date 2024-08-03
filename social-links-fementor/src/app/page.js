3d6c593aebd0fa16340136b2f598245f3fa4949c
Tremckinley <tremckinley901@gmail.com>
2024-08-02 23:14:31 -0500

    added project files

--- social-links-fementor/src/app/page.js
+++ social-links-fementor/src/app/page.js
@@ -2,7 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello World!</div>
-    
+    <div>
+
+      Jessica Randall
+      London, United Kingdom
+      "Front-end developer and avid reader."
+
+      GitHub
+      Frontend Mentor
+      LinkedIn
+      Twitter
+      Instagram
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+
   );
 }

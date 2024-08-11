afbc78a7e7162999363d5a9b672864a2dd808850
Tremckinley <tremckinley901@gmail.com>
2024-08-10 20:49:56 -0500

    Added partial card

--- blog-preview-card-fementor/src/app/page.js
+++ blog-preview-card-fementor/src/app/page.js
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import BlogImage from "../../assets/images/blogimage";
 
 export default function Home() {
-  return 
+  return (
+  <div className="flex h-screen bg-yellow-300 w-full">
+    <div id="card" className="flex flex-col items-center h-fit bg-white p-6 w-full max-w-[384px] border border-black rounded-lg">
+      <BlogImage />
+      <div id='content' className="flex flex-col border border-red-800 w-full m-6">
+        <div id='category' className="flex w-fit p-1 font-bold text-sm bg-yellow-300 rounded-lg text-center">Learning</div>
+        <div className="my-6 text-sm">Published 21 Dec 2023</div>
+      </div>
 
-}
+    </div>
+  </div>
+  
+)}
